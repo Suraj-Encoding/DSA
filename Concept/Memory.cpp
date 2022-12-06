d90a7dae5b87390132a34d198e8636a7a84ec609
Suraj Dalvi <108187430+Suraj-Encoding@users.noreply.github.com>
2022-12-06 07:56:31 +0530

    Add files via upload

---
+++ Concept/Memory.cpp
@@ -0,0 +1,51 @@
+// # Dynamic Memory Allocation in C++ #
+// The program will show the use of 'new' and 'delete' operator or keyword in c++
+#include <iostream>  
+using namespace std; 
+
+int main ()  
+{  
+    // Pointer initialization to null  
+    int* m = NULL;  
+  
+    // Request memory for the variable  
+    // using new operator  
+    m = new(nothrow) int;  
+    if (!m)  
+        cout<< "allocation of memory failed\n";  
+    else  
+    {  
+        // Store value at allocated address  
+        *m=29;  
+        cout<< "Value of m: " << *m <<endl;  
+    }  
+
+    // Request block of memory  
+    // using new operator  
+    float *f = new(nothrow) float(75.25);  
+    cout<< "Value of f: " << *f <<endl;  
+    
+    // Request block of memory of size   
+    int size = 5;  
+    int *arr = new(nothrow) int[size];  
+    if (!arr)  
+        cout<< "allocation of memory failed\n";  
+    else  
+    {  
+        for (int i = 0; i< size; i++)  
+            arr[i] = i+1;  
+  
+        cout<< "Value store in block of memory: ";  
+        for (int i = 0; i< size; i++)  
+            cout<<arr[i] << " ";  
+    }  
+    cout<<endl;
+
+    // freed the allocated memory  
+    delete m;  
+    delete f;  
+    // freed the block of allocated memory  
+    delete[] arr;  
+  
+    return 0;  
+}  
